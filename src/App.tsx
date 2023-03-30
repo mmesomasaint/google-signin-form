b783729fc7b4bd8c15bab6ff49f681d68b279018
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-03-30 06:43:47 +0100

    Start design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className='pt-20'>
-      <div className='p-20 w-fit mx-auto rounded-2xl border border-gray-400 bg-white'>
-        
+      <div className='p-16 w-fit mx-auto rounded-2xl border border-gray-400 bg-white'>
+        Sign in
       </div>
     </div>
   )
