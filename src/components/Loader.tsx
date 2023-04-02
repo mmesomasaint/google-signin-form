d410965a8de2026e318a69ad45dc3db4521e72b7
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-04-02 05:01:19 +0100

    Adjust loader height

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -7,7 +7,7 @@ function Loader({ loading }: LoadProps) {
     <div
       className={`${
         loading ? 'block' : 'hidden'
-      } absolute w-full h-2 top-0 left-0 z-10`}
+      } absolute w-full h-1 top-0 left-0 z-10 bg-gray-300`}
     >
       <div className="w-full h-full bg-blue-700 relative overflow-hidden animate-load" />
     </div>
