9c9b004f4b40c22c10c5b8f9cf2f234197cc6193
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-04-02 04:50:14 +0100

    Create animation in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        load: 'load 1s infinite',
+      },
+      keyframes: {
+        load: {
+          '0%': {
+            transform: 'translateX(-100%)',
+          },
+          '50%': {
+            transform: 'translateX(100%)',
+          },
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
