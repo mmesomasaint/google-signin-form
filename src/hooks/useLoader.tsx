7cdacff050625e61520226aa624ee5af39bbb281
mmesomasaint <104903171+mmesomasaint@users.noreply.github.com>
2023-04-02 04:42:08 +0100

    Design load-spinner

---
+++ src/hooks/useLoader.tsx
@@ -0,0 +1,19 @@
+type LoadProps = {
+  loading: boolean;
+};
+
+function useLoader({ loading }: LoadProps) {
+  return {
+    loader: () => (
+      <div
+        className={`${
+          loading ? 'fixed' : 'hidden'
+        } w-full h-2 fixed top-0 left-0 z-10`}
+      >
+        <div className="w-full h-full bg-blue-500 relative overflow-hidden animate-load" />
+      </div>
+    ),
+  };
+}
+
+export default useLoader;
